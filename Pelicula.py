81c269d606c53df3160e582941813bcbc047e3dd
Nacho828 <nchomaxanton@gmail.com>
2025-03-10 12:19:34 +0100

    cambios

---
+++ Pelicula.py
@@ -0,0 +1,11 @@
+class Pelicula:
+# Constructor de clase
+    def __init__(self, titulo, duracion, lanzamiento):
+        self.titulo = titulo
+        self.duracion = duracion
+        self.lanzamiento = lanzamiento
+        
+
+def __str__(self):
+    print('Se ha creado la película:', self .titulo)
+    return '{} ({})'.format(self.titulo, self.lanzamiento)
---
+++ main.py
@@ -0,0 +1,11 @@
+p = Pelicula("El Padrino", 175, 1972)
+c = Catalogo([p]) # Añado una lista con una película desde el
+principio
+c.mostrar()
+c.agregar(Pelicula("El Padrino: Parte 2", 202, 1974)) # Añadimos otra
+c.mostrar()
+Se ha creado la película: El Padrino
+El Padrino (1972)
+Se ha creado la película: El Padrino: Parte 2
+El Padrino (1972)
+El Padrino: Parte 2 (1974)
