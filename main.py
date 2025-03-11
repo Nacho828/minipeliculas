97e5286763df361a740cfa0ae2cef50949268989
Nacho828 <nchomaxanton@gmail.com>
2025-03-11 09:28:42 +0100

    cambios 2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-p = Pelicula("El Padrino", 175, 1972)
-c = Catalogo([p]) # Añado una lista con una película desde el
-principio
-c.mostrar()
-c.agregar(Pelicula("El Padrino: Parte 2", 202, 1974)) # Añadimos otra
-c.mostrar()
-Se ha creado la película: El Padrino
-El Padrino (1972)
-Se ha creado la película: El Padrino: Parte 2
-El Padrino (1972)
-El Padrino: Parte 2 (1974)
+from Pelicula import Pelicula
+from Catalogo import Catalogo
+
+def main():
+    peliculas = [
+        Pelicula("El Padrino", "Francis Ford Coppola", 1972),
+        Pelicula("El Caballero Oscuro", "Christopher Nolan", 2008),
+        Pelicula("Pulp Fiction", "Quentin Tarantino", 1994)
+    ]
+    
+    catalogo = Catalogo()
+    catalogo.mostrar_catalogo()
+    
+    
+
+if __name__ == "__main__":
+    main()
